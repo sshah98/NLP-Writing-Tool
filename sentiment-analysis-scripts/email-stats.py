08c2c06d2963a415403698d6448f6fa70f9a44e4
Suraj <shahsuraj261@gmail.com>
2017-12-08 15:51:11 -0500

    edited file

--- sentiment-analysis-scripts/email-stats.py
+++ sentiment-analysis-scripts/email-stats.py
@@ -49,10 +49,10 @@ class EmailStats(object):
         return self.text_analyzer.entities_text(self.string)
 
     def complex_words(self):
-
+        
         # returns list of words with multiple syllables
         return textstat.difficult_words(self.string)
-        
+    
 string = 'I am happy today'
 myobj = EmailStats(string)
 print(myobj.get_text_easiness())
