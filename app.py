4cf8ebaf78c4c98e75adbfc438b4ddd48a65e642
sshah98 <shahsuraj261@gmail.com>
2018-07-22 14:43:38 -0400

    changed many files

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'nlp-analysis-key.json'
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
 
 from google_npl import GoogleNLP
 from email_stats import EmailStats
@@ -16,19 +16,27 @@ def index():
 
     if request.method == "POST":
 
-        user_text = request.form['writing']
-        word_count = EmailStats(user_text).word_count()
-        get_text_easiness = EmailStats(user_text).get_text_easiness()
-        sentence_count = EmailStats(user_text).sentence_count()
-        subjectivity = EmailStats(user_text).subjectivity()
-        complex_words = EmailStats(user_text).complex_words()
-        # print(sentiment)
-        sentiment = GoogleNLP(user_text).sentiment_text()
-        print(sentiment)
-        
-        
+        try:
 
-        return render_template('index.html', results=0)
+            user_text = request.form['writing']
+            word_count = EmailStats(user_text).word_count()
+            get_text_easiness = EmailStats(user_text).get_text_easiness()
+            sentence_count = EmailStats(user_text).sentence_count()
+            subjectivity = EmailStats(user_text).subjectivity()
+            complex_words = EmailStats(user_text).complex_words()
+            # print(sentiment)
+            sentiment = GoogleNLP(user_text).sentiment_text()
+            sentiment_score, sentiment_mag = sentiment.split(' ')
+            
+            
+        
+            return render_template('index.html', results=[('Word Count', word_count), ('Sentence Count', sentence_count), ('Readability', get_text_easiness), ('Subjectivity', subjectivity), ('Complex Words', complex_words), ('Sentiment Score', sentiment_score), ('Sentiment Strength', sentiment_mag)])
+        
+        except:
+            flash('Error')
+            return render_template('index.html')
+        
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
